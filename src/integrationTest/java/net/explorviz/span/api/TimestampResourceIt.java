25b93e9ebd64d6430bd04f03145aff489de417a6
Alexander Krause <akr@informatik.uni-kiel.de>
2024-04-11 10:47:13 +0200

    testing query param newer in timestamp api

--- src/integrationTest/java/net/explorviz/span/api/TimestampResourceIt.java
+++ src/integrationTest/java/net/explorviz/span/api/TimestampResourceIt.java
@@ -8,7 +8,9 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.common.mapper.TypeRef;
 import io.restassured.response.Response;
 import jakarta.inject.Inject;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import net.explorviz.span.kafka.KafkaTestResource;
@@ -39,7 +41,7 @@ public class TimestampResourceIt {
 
     final UUID uuidExpected = UUID.randomUUID();
 
-    final PersistenceSpan differentTokenSpan =
+    final PersistenceSpan firstSpanOfFirstBucket =
         new PersistenceSpan(UUID.randomUUID(), gitCommitChecksum, "123L", "",
             "1L", startEarly, endEarly, "nodeIp", "app-name", "java", 0,
             "net.explorviz.Class.myMethod()", "847");
@@ -47,28 +49,26 @@ public class TimestampResourceIt {
     final String duplicateMethodName = "myMethodName()";
     final String otherMethodName = "myOtherMethodName()";
 
-    final PersistenceSpan firstOccurenceSpan =
+    final PersistenceSpan firstSpanOfSecondBuckec =
         new PersistenceSpan(uuidExpected, gitCommitChecksum, "123L", "", "1L", startEarly, endEarly,
             "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
 
-    final PersistenceSpan secondOccurenceSpan =
+    final PersistenceSpan firstSpanOfThirdBucket =
         new PersistenceSpan(uuidExpected, gitCommitChecksum, "789L", "", "3L", startLate, endLate,
             "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
 
-    final PersistenceSpan otherSpan =
+    final PersistenceSpan secondSpanOfSecondBucket =
         new PersistenceSpan(uuidExpected, gitCommitChecksum, "456L", "0L", "", startExpected,
             endExpected, "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + otherMethodName,
             "321");
 
-    spanProcessor.accept(differentTokenSpan);
-    spanProcessor.accept(firstOccurenceSpan);
-    spanProcessor.accept(secondOccurenceSpan);
-    spanProcessor.accept(otherSpan);
+    spanProcessor.accept(firstSpanOfFirstBucket);
+    spanProcessor.accept(firstSpanOfSecondBuckec);
+    spanProcessor.accept(firstSpanOfThirdBucket);
+    spanProcessor.accept(secondSpanOfSecondBucket);
 
     final Response response = given().pathParam("token", uuidExpected).when()
         .get("/v2/landscapes/{token}/timestamps");
-
-    System.out.println("ALEX HIER: " + response.getBody().asPrettyString());
 
     final List<Timestamp> resultList = response.getBody().as(new TypeRef<List<Timestamp>>() {
     });
@@ -77,26 +77,102 @@ public class TimestampResourceIt {
     Assertions.assertEquals(2, resultList.size());
 
     // Check that there are the correct timestamp buckets with correct span count
-    Optional<Timestamp> optionalTimestamp =
-        resultList.stream().filter(timestamp -> timestamp.epochMilli() == 1702545560000L)
-            .findFirst();
+    // Check that there are the correct timestamp buckets with correct span count
+    Map<Long, Integer> doubleBraceMap = new HashMap<Long, Integer>() {{
+      put(1702545560000L, 2);
+      put(1702545570000L, 1);
+    }};
 
-    if (optionalTimestamp.isEmpty()) {
-      Assertions.fail(
-          "Found no timestamp for time bucket 1702545560000L, but there should be one.");
-    } else {
-      Assertions.assertEquals(optionalTimestamp.get().spanCount(), 2);
+    for (final Map.Entry<Long, Integer> entry : doubleBraceMap.entrySet()) {
+      long key = entry.getKey().longValue();
+      int value = entry.getValue().intValue();
+
+      Optional<Timestamp> optionalTimestamp =
+          resultList.stream().filter(timestamp -> timestamp.epochMilli() == key)
+              .findFirst();
+
+      if (optionalTimestamp.isEmpty()) {
+        Assertions.fail(
+            "Found no timestamp for time bucket " + key + ", but there should be one.");
+      } else {
+        Assertions.assertEquals(value, optionalTimestamp.get().spanCount());
+      }
     }
+  }
 
-    optionalTimestamp =
-        resultList.stream().filter(timestamp -> timestamp.epochMilli() == 1702545570000L)
-            .findFirst();
+  @Test
+  void testLoadNewerTimestampsForToken() {
+    final long firstBucketStart = 1702545554404L;
+    final long firstBucketEnd = firstBucketStart + 1000;
+    final long secondBucketStart = 1702545564404L;
+    final long secondBucketEnd = secondBucketStart + 1000;
+    final long thirdBucketStart = secondBucketStart + 10_000;
+    final long thirdBucketEnd = thirdBucketStart + 1000;
 
-    if (optionalTimestamp.isEmpty()) {
-      Assertions.fail(
-          "Found no timestamp for time bucket 1702545570000L, but there should be one.");
-    } else {
-      Assertions.assertEquals(optionalTimestamp.get().spanCount(), 1);
+    final UUID uuidExpected = UUID.randomUUID();
+
+    final PersistenceSpan firstSpanOfFirstBucket =
+        new PersistenceSpan(uuidExpected, gitCommitChecksum, "0123L", "",
+            "1L", firstBucketStart, firstBucketEnd, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class.myMethod()", "847");
+
+    final String duplicateMethodName = "myMethodName()";
+    final String otherMethodName = "myOtherMethodName()";
+
+    final PersistenceSpan firstSpanOfSecondBuckec =
+        new PersistenceSpan(uuidExpected, gitCommitChecksum, "123L", "", "1L", secondBucketStart,
+            secondBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan firstSpanOfThirdBucket =
+        new PersistenceSpan(uuidExpected, gitCommitChecksum, "789L", "", "3L", thirdBucketStart,
+            thirdBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan secondSpanOfSecondBucket =
+        new PersistenceSpan(uuidExpected, gitCommitChecksum, "456L", "0L", "", secondBucketStart,
+            secondBucketEnd, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class." + otherMethodName,
+            "321");
+
+    spanProcessor.accept(firstSpanOfFirstBucket);
+    spanProcessor.accept(firstSpanOfSecondBuckec);
+    spanProcessor.accept(firstSpanOfThirdBucket);
+    spanProcessor.accept(secondSpanOfSecondBucket);
+
+    final Response response =
+        given().pathParam("token", uuidExpected).queryParam("newest", firstBucketStart - 10000)
+            .when()
+            .get("/v2/landscapes/{token}/timestamps");
+
+    final List<Timestamp> resultList = response.getBody().as(new TypeRef<List<Timestamp>>() {
+    });
+
+    //System.out.println("HIER DA " + Arrays.deepToString(resultList.toArray()));
+
+    Assertions.assertEquals(3, resultList.size());
+
+    // Check that there are the correct timestamp buckets with correct span count
+    Map<Long, Integer> doubleBraceMap = new HashMap<Long, Integer>() {{
+      put(1702545550000L, 1);
+      put(1702545560000L, 2);
+      put(1702545570000L, 1);
+    }};
+
+    for (Map.Entry<Long, Integer> entry : doubleBraceMap.entrySet()) {
+      long key = entry.getKey().longValue();
+      int value = entry.getValue().intValue();
+
+      Optional<Timestamp> optionalTimestamp =
+          resultList.stream().filter(timestamp -> timestamp.epochMilli() == key)
+              .findFirst();
+
+      if (optionalTimestamp.isEmpty()) {
+        Assertions.fail(
+            "Found no timestamp for time bucket " + key + ", but there should be one.");
+      } else {
+        Assertions.assertEquals(value, optionalTimestamp.get().spanCount());
+      }
     }
   }
 
