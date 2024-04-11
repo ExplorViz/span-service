b12361bd9842d3a2200e657216204970c2f8cfd8
Alexander Krause <akr@informatik.uni-kiel.de>
2024-04-11 13:44:47 +0200

    test commit query parameter for timestamp api

--- src/integrationTest/java/net/explorviz/span/api/TimestampResourceIt.java
+++ src/integrationTest/java/net/explorviz/span/api/TimestampResourceIt.java
@@ -78,12 +78,12 @@ public class TimestampResourceIt {
 
     // Check that there are the correct timestamp buckets with correct span count
     // Check that there are the correct timestamp buckets with correct span count
-    Map<Long, Integer> doubleBraceMap = new HashMap<Long, Integer>() {{
+    Map<Long, Integer> expectedValuesMap = new HashMap<Long, Integer>() {{
       put(1702545560000L, 2);
       put(1702545570000L, 1);
     }};
 
-    for (final Map.Entry<Long, Integer> entry : doubleBraceMap.entrySet()) {
+    for (final Map.Entry<Long, Integer> entry : expectedValuesMap.entrySet()) {
       long key = entry.getKey().longValue();
       int value = entry.getValue().intValue();
 
@@ -153,13 +153,140 @@ public class TimestampResourceIt {
     Assertions.assertEquals(3, resultList.size());
 
     // Check that there are the correct timestamp buckets with correct span count
-    Map<Long, Integer> doubleBraceMap = new HashMap<Long, Integer>() {{
+    Map<Long, Integer> expectedValuesMap = new HashMap<Long, Integer>() {{
       put(1702545550000L, 1);
       put(1702545560000L, 2);
       put(1702545570000L, 1);
     }};
 
-    for (Map.Entry<Long, Integer> entry : doubleBraceMap.entrySet()) {
+    testResultListAgainstExpectedValues(resultList, expectedValuesMap);
+  }
+
+  @Test
+  void testLoadAllTimestampsForTokenAndCommit() {
+    final long firstBucketStart = 1702545554404L;
+    final long firstBucketEnd = firstBucketStart + 1000;
+    final long secondBucketStart = 1702545564404L;
+    final long secondBucketEnd = secondBucketStart + 1000;
+    final long thirdBucketStart = secondBucketStart + 10_000;
+    final long thirdBucketEnd = thirdBucketStart + 1000;
+
+    final UUID uuidExpected = UUID.randomUUID();
+    final String expectedCommit = "testCommit";
+
+    final PersistenceSpan firstSpanOfFirstBucket =
+        new PersistenceSpan(uuidExpected, "notTestCommit-1", "0123L", "",
+            "1L", firstBucketStart, firstBucketEnd, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class.myMethod()", "847");
+
+    final String duplicateMethodName = "myMethodName()";
+    final String otherMethodName = "myOtherMethodName()";
+
+    final PersistenceSpan firstSpanOfSecondBuckec =
+        new PersistenceSpan(uuidExpected, expectedCommit, "123L", "", "1L", secondBucketStart,
+            secondBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan firstSpanOfThirdBucket =
+        new PersistenceSpan(uuidExpected, "notTestCommit-2", "789L", "", "3L", thirdBucketStart,
+            thirdBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan secondSpanOfSecondBucket =
+        new PersistenceSpan(uuidExpected, "notTestCommit-2", "456L", "0L", "", secondBucketStart,
+            secondBucketEnd, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class." + otherMethodName,
+            "321");
+
+    spanProcessor.accept(firstSpanOfFirstBucket);
+    spanProcessor.accept(firstSpanOfSecondBuckec);
+    spanProcessor.accept(firstSpanOfThirdBucket);
+    spanProcessor.accept(secondSpanOfSecondBucket);
+
+    final Response response =
+        given().pathParam("token", uuidExpected).queryParam("commit", expectedCommit)
+            .when()
+            .get("/v2/landscapes/{token}/timestamps");
+
+    final List<Timestamp> resultList = response.getBody().as(new TypeRef<List<Timestamp>>() {
+    });
+
+    Assertions.assertEquals(1, resultList.size());
+
+    // Check that there are the correct timestamp buckets with correct span count
+    Map<Long, Integer> expectedValuesMap = new HashMap<Long, Integer>() {{
+      put(1702545560000L, 1);
+    }};
+
+    testResultListAgainstExpectedValues(resultList, expectedValuesMap);
+  }
+
+  @Test
+  void testLoadNewerTimestampsForTokenAndCommit() {
+    final long firstBucketStart = 1702545554404L;
+    final long firstBucketEnd = firstBucketStart + 1000;
+    final long secondBucketStart = 1702545564404L;
+    final long secondBucketEnd = secondBucketStart + 1000;
+    final long thirdBucketStart = secondBucketStart + 10_000;
+    final long thirdBucketEnd = thirdBucketStart + 1000;
+
+    final UUID uuidExpected = UUID.randomUUID();
+    final String expectedCommit = "testCommit";
+
+    final PersistenceSpan firstSpanOfFirstBucket =
+        new PersistenceSpan(uuidExpected, expectedCommit, "0123L", "",
+            "1L", firstBucketStart, firstBucketEnd, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class.myMethod()", "847");
+
+    final String duplicateMethodName = "myMethodName()";
+    final String otherMethodName = "myOtherMethodName()";
+
+    final PersistenceSpan firstSpanOfSecondBuckec =
+        new PersistenceSpan(uuidExpected, expectedCommit, "123L", "", "1L", secondBucketStart,
+            secondBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan firstSpanOfThirdBucket =
+        new PersistenceSpan(uuidExpected, expectedCommit, "789L", "", "3L", thirdBucketStart,
+            thirdBucketEnd,
+            "nodeIp", "app-name", "java", 0, "net.explorviz.Class." + duplicateMethodName, "847");
+
+    final PersistenceSpan secondSpanOfSecondBucket =
+        new PersistenceSpan(uuidExpected, expectedCommit, "456L", "0L", "",
+            secondBucketStart + 1000,
+            secondBucketEnd + 1000, "nodeIp", "app-name", "java", 0,
+            "net.explorviz.Class." + otherMethodName,
+            "321");
+
+    spanProcessor.accept(firstSpanOfFirstBucket);
+    spanProcessor.accept(firstSpanOfSecondBuckec);
+    spanProcessor.accept(firstSpanOfThirdBucket);
+    spanProcessor.accept(secondSpanOfSecondBucket);
+
+    final Response response =
+        given().pathParam("token", uuidExpected)
+            .queryParam("newest", secondBucketEnd - 10000)
+            .queryParam("commit", expectedCommit)
+            .when()
+            .get("/v2/landscapes/{token}/timestamps");
+
+    final List<Timestamp> resultList = response.getBody().as(new TypeRef<List<Timestamp>>() {
+    });
+
+    Assertions.assertEquals(2, resultList.size());
+
+    // Check that there are the correct timestamp buckets with correct span count
+    Map<Long, Integer> expectedValuesMap = new HashMap<Long, Integer>() {{
+      put(1702545560000L, 2);
+      put(1702545570000L, 1);
+    }};
+
+    testResultListAgainstExpectedValues(resultList, expectedValuesMap);
+  }
+
+  private void testResultListAgainstExpectedValues(final List<Timestamp> resultList,
+      final Map<Long, Integer> expectedValuesMap) {
+    for (Map.Entry<Long, Integer> entry : expectedValuesMap.entrySet()) {
       long key = entry.getKey().longValue();
       int value = entry.getValue().intValue();
 
