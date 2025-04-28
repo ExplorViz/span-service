37242037993e6e05c71ad53024dc0a13e34d50b9
Malte Hansen <malte.hanseat@gmail.com>
2025-04-28 12:24:47 +0200

    WIP: Switch from milli to nanoseconds

--- src/main/java/net/explorviz/span/adapter/service/converter/SpanConverterImpl.kt
+++ src/main/java/net/explorviz/span/adapter/service/converter/SpanConverterImpl.kt
@@ -7,30 +7,26 @@ import net.explorviz.avro.Span
 @ApplicationScoped
 class SpanConverterImpl : SpanConverter<Span> {
 
-    private companion object {
-        const val TO_MILLISEC_DIVISOR = 1_000_000L
-    }
-
     override fun fromOpenTelemetrySpan(ocSpan: io.opentelemetry.proto.trace.v1.Span): Span {
 
         val attributesReader = AttributesReader(ocSpan)
 
         val parentSpan =
-            if (ocSpan.parentSpanId.size() > 0) {
-                IdHelper.convertSpanId(ocSpan.parentSpanId.toByteArray())
-            } else {
-                ""
-            }
+                if (ocSpan.parentSpanId.size() > 0) {
+                    IdHelper.convertSpanId(ocSpan.parentSpanId.toByteArray())
+                } else {
+                    ""
+                }
 
         val span =
-            Span.newBuilder()
-                .setLandscapeToken(attributesReader.landscapeToken)
-                .setGitCommitChecksum(attributesReader.gitCommitChecksum)
-                .setParentSpanId(parentSpan)
-                .setSpanId(IdHelper.convertSpanId(ocSpan.spanId.toByteArray()))
-                .setTraceId(IdHelper.convertTraceId(ocSpan.traceId.toByteArray()))
-                .setStartTimeEpochMilli(ocSpan.startTimeUnixNano / TO_MILLISEC_DIVISOR)
-                .setEndTimeEpochMilli(ocSpan.endTimeUnixNano / TO_MILLISEC_DIVISOR)
+                Span.newBuilder()
+                        .setLandscapeToken(attributesReader.landscapeToken)
+                        .setGitCommitChecksum(attributesReader.gitCommitChecksum)
+                        .setParentSpanId(parentSpan)
+                        .setSpanId(IdHelper.convertSpanId(ocSpan.spanId.toByteArray()))
+                        .setTraceId(IdHelper.convertTraceId(ocSpan.traceId.toByteArray()))
+                        .setStartTimeEpochNano(ocSpan.startTimeUnixNano)
+                        .setEndTimeEpochNano(ocSpan.endTimeUnixNano)
 
         attributesReader.appendToSpan(span)
 
