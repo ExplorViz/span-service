bd98c1738612a556bc8864687551862d1cca4d09
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-04-26 12:27:28 +0200

    Add missing check for DEFAULT_GIT_COMMIT_CHECKSUM value

--- src/main/java/net/explorviz/span/messaging/KafkaSpanExporter.java
+++ src/main/java/net/explorviz/span/messaging/KafkaSpanExporter.java
@@ -4,6 +4,7 @@ import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import net.explorviz.persistence.avro.SpanData;
+import net.explorviz.span.adapter.service.converter.DefaultAttributeValues;
 import net.explorviz.span.persistence.PersistenceSpan;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
@@ -26,23 +27,23 @@ public class KafkaSpanExporter {
   }
 
   private SpanData mapToAvro(PersistenceSpan span) {
+    final boolean hasGitCommitChecksum =
+        !span.gitCommitChecksum().equals(DefaultAttributeValues.DEFAULT_GIT_COMMIT_CHECKSUM)
+            && !span.gitCommitChecksum().isBlank();
+
     return SpanData.newBuilder()
         .setSpanId(span.spanId())
         .setParentId(span.parentSpanId())
         .setTraceId(span.traceId())
-        .setLandscapeTokenId(span.landscapeToken().toString())
+        .setLandscapeTokenId(span.landscapeToken())
         .setStartTime(span.startTime())
         .setEndTime(span.endTime())
         .setApplicationName(span.applicationName())
         .setLanguage(span.applicationLanguage())
         .setFunctionName(span.functionName())
         .setFilePath(span.filePath())
-        .setClassName(
-            span.className().isBlank() ? null : span.className())
-        .setCommitHash(
-            span.gitCommitChecksum().isBlank()
-                ? null
-                : span.gitCommitChecksum())
+        .setClassName(span.className().isBlank() ? null : span.className())
+        .setCommitHash(hasGitCommitChecksum ? span.gitCommitChecksum() : null)
         .build();
   }
 }
